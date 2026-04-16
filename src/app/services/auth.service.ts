ed4517d53c461b3ad628abcd2ff0e9e02ca1e9c5
harikrishnangm6748 <harikrishnan.10840768@Ltimindtree.com>
2026-04-16 09:00:55 +0000

    angular fixed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,7 +25,7 @@ export interface LoginResponse {
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000/api';
+  private apiUrl = 'http://localhost:5048/api';
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
